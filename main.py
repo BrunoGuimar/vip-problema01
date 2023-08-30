be8d689e7e47903d8fb3f805e0fd440f91c71fa5
Bruno Guimarães <brunoguima13@gmail.com>
2023-08-30 11:52:43 -0300

    Tornando variáveis do construtor da classe privadas

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 class Moeda:
     def __init__(self, valor: str, tipo_moeda: str, caractere: str):
-        self.valor = valor
-        self.tipo_moeda = tipo_moeda
-        self.caractere = caractere
+        self._valor = valor
+        self._tipo_moeda = tipo_moeda
+        self._caractere = caractere
 
     # Retorna a string formatada {valor} no tipo float
     def get_valor(self):
-        valor_formatado = self.valor.replace(",", "").replace(".", "").replace("R$", "").replace("$", "").strip()
+        valor_formatado = self._valor.replace(",", "").replace(".", "").replace("R$", "").replace("$", "").strip()
         return float(int(valor_formatado) / 100)
 
     # Recebe o tipo e caractere da moeda, substituindo pelos valores passados no parâmetro da função
@@ -17,7 +17,7 @@ class Moeda:
     # Função privada responsável por formatar a moeda de acordo com os caracteres e o tipo informado
     # Implementada em casos mais flexíveis de valores
     def _formatar_moeda(self, tipo_moeda, caractere):
-        moeda_formatada = self.valor.replace("R$", "").replace("$", "")
+        moeda_formatada = self._valor.replace("R$", "").replace("$", "")
         if tipo_moeda == "R$":  # "," VIRA "." - CASA DECIMAL == ","
             moeda_formatada = moeda_formatada.replace(",", ".")
             return f"{tipo_moeda}{moeda_formatada[:len(moeda_formatada) - 3]}{caractere}{moeda_formatada[len(moeda_formatada) - 2:]}"
